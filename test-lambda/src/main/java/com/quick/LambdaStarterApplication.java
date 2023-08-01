7f480f8acc98efc9c2e12b0aad12fdcd5645554d
hanfucheng <781507157@qq.com>
2023-08-02 00:04:03 +0800

    新增模块lambda

---
+++ test-lambda/src/main/java/com/quick/LambdaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaStarterApplication.class, args);
+	}
+
+}
